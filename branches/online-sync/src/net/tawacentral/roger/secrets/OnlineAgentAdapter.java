79ca1f2c413eaa3ca230339c5eb5f4c0a6c53c91
ceperman@gmail.com <ceperman@gmail.com@1d24a8dc-1b16-11de-ac9f-1d9250355bca>
2012-11-24 17:04:55 +0000

    Remove need to configure OSA

--- branches/online-sync/src/net/tawacentral/roger/secrets/OnlineAgentAdapter.java
+++ branches/online-sync/src/net/tawacentral/roger/secrets/OnlineAgentAdapter.java
@@ -6,51 +6,30 @@ import android.content.Context;
 import android.widget.ArrayAdapter;
 
 /**
- * Provides a filtered list of OnlineSecretsAgents.
- * The filter criterion is configured (yes/no)
+ * Provides a list of available OnlineSecretsAgents.
  * 
  * @author Chris Wood
  */
 public class OnlineAgentAdapter extends ArrayAdapter<OnlineSyncAgent> {
 	
-	private Context context;
-	private boolean showAllApps; // true if all apps should be shown so that
-																// they can be selected for configuring
-																// otherwise only configured apps will be shown
-
 	/**
-	 * Creates a new adapter for the indicated mode.
+	 * Creates a new adapter.
 	 * 
 	 * @param context
 	 * @param resource
 	 * @param textViewResourceId
-	 * @param configMode
-	 *          what OSAs should be included: true = all, false = configured only
 	 */
   public OnlineAgentAdapter(Context context, int resource,
-          int textViewResourceId, boolean configMode) {
+          int textViewResourceId) {
     super(context, resource, textViewResourceId);
-    this.context = context;
-    this.showAllApps = configMode;
   }
 	
 	/**
-   * Update the adapter's list of filtered OSAs
-	 * @param secretsList the list of secrets
+   * Update the adapter's list of available OSAs
    */
-  public void updateAppList(SecretsListAdapter secretsList) {
+  public void updateAppList() {
     clear();
-    if (showAllApps) {
-      addAllAgents(OnlineAgentManager.getInstalledAgents());
-    } else {
-      // add suitable configured OSAs to the list
-      addAllAgents(OnlineAgentManager.getConfiguredAgents());
-      // only show the configure option if at least 1 OSA is installed.
-      if (OnlineAgentManager.getInstalledAgents().size() > 0) {
-        add(new OnlineSyncAgent(
-                context.getString(R.string.list_osa_configure), "configure"));
-      }
-    }
+    addAllAgents(OnlineAgentManager.getAvailableAgents());
   }
   
   /*
