0c537f96268ab8da424aba73fbb254a09f40729b
ceperman@gmail.com <ceperman@gmail.com@1d24a8dc-1b16-11de-ac9f-1d9250355bca>
2013-08-17 05:12:35 +0000

    Move responsibility for key generation to OnlineAgentManager

--- branches/online-sync/src/net/tawacentral/roger/secrets/OnlineSyncAgent.java
+++ branches/online-sync/src/net/tawacentral/roger/secrets/OnlineSyncAgent.java
@@ -1,8 +1,7 @@
 package net.tawacentral.roger.secrets;
 
-import java.security.SecureRandom;
-
 import net.tawacentral.roger.secrets.OnlineAgentManager.SecretsReceivedListener;
+
 
 /**
  * Represents an Online Sync Agent application.
@@ -10,25 +9,17 @@ import net.tawacentral.roger.secrets.OnlineAgentManager.SecretsReceivedListener;
  * @author Chris Wood
  */
 public class OnlineSyncAgent {
-  private String displayName;
-  private String classId;
-
-  /*
-   * The response key is a randomly generated string that is provided to the OSA
-   * as part of the sync request and must be returned in the response in order
-   * for it to be accepted. The key is changed when the response is received to
-   * ensure that any subsequent or unsolicited responses are rejected.
-   */
-  private static final int RESPONSEKEY_LENGTH = 8;
-  private String responseKey;
-
-  /*
-   * "available" indicates if that agent is available. Agents are required to
-   * respond to a roll call broadcast every time the app is resumed. Previously
-   * known agents that do not respond are marked as not available but remain
-   * configured.
-   */
-  private boolean available = true;
+	private String displayName;
+	private String classId;
+	
+	/*
+	 * The response key is a randomly generated string that is provided to the
+	 * OSA as part of the sync request and must be returned in the response in
+	 * order for it to be accepted. The key is changed when the response is
+	 * received to ensure that any subsequent or unsolicited responses are
+	 * rejected.
+	 */
+	private String responseKey;
 
   private SecretsReceivedListener listener;
 
@@ -62,25 +53,6 @@ public class OnlineSyncAgent {
   }
 
   /**
-   * Test the available indicator
-   *
-   * @return available
-   */
-  public boolean isAvailable() {
-    return available;
-  }
-
-  /**
-   * Set the available indicator
-   *
-   * @param available
-   *          the indicator to set
-   */
-  public void setAvailable(boolean available) {
-    this.available = available;
-  }
-
-  /**
    * Get the response key
    *
    * @return the responseKey
@@ -90,13 +62,11 @@ public class OnlineSyncAgent {
   }
 
   /**
-   * Generate a new response key
+   * set the response key
+   * @param key the response key to set
    */
-  public void generateResponseKey() {
-    SecureRandom random = new SecureRandom();
-    byte[] keyBytes = new byte[RESPONSEKEY_LENGTH];
-    random.nextBytes(keyBytes);
-    this.responseKey = new String(keyBytes);
+  public void setResponseKey(String key) {
+    responseKey = key;
   }
 
   /**
